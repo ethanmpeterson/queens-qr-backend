a7501967eeb803e098230a6328b011287be175fd
Ethan Peterson <17emp5@queensu.ca>
2019-01-15 12:32:11 -0500

    set up mongodb

---
+++ db.js
@@ -0,0 +1,5 @@
+// MONGO URI
+// mongodb://clunkyeman:apsc100m3@ds157544.mlab.com:57544/apscm3
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://clunkyeman:apsc100m3@ds157544.mlab.com:57544/apscm3');
