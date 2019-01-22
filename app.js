306cbb503cadc057d77e5ada8a0d24c1a46d9d27
Ethan Peterson <17emp5@queensu.ca>
2019-01-22 10:54:21 -0500

    added env port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var db = require('./db');
 
-var port = 3000
+var port = process.env.PORT || 3000;
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 
