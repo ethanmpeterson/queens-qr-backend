082ce9fdc414ba6fbf1e7e9c8cf98d7e967074d4
Ethan Peterson <ethan@petetech.net>
2019-01-31 16:18:41 -0500

    fixed #2

--- db.js
+++ db.js
@@ -2,5 +2,4 @@ var mongoose = require('mongoose');
 const usr = process.env.DB_USER;
 const pwd = process.env.DB_PWD;
 
-
 mongoose.connect("mongodb://" + usr + ":" + pwd + "@ds157544.mlab.com:57544/apscm3");
